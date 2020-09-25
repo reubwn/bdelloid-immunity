1023585764223235f3da007726d6796f7cb80135
Reuben Nowell <reubennowell@gmail.com>
2020-09-25 16:08:28 +0200

    update

--- collate_DE_results.pl
+++ collate_DE_results.pl
@@ -48,6 +48,7 @@ die $usage unless ($transcripts_file);
 my %features_hash;
 
 ## parse feature names from fasta
+print STDERR "[INFO] Parsing sequences in file: $transcripts_file\n";
 my $in = Bio::SeqIO -> new ( -file => $transcripts_file, -format => 'fasta' );
 while ( my $seq_obj = $in->next_seq() ) {
   $features_hash{$seq_obj->display_id()}{length} = $seq_obj->length();
@@ -63,26 +64,41 @@ foreach my $current_DE_file (@DE_files) {
     next if $. == 1; ## header
     chomp $line;
     my @F = split (m/\s+/, $line);
-    # if ( exists($features_hash{$F[0]}) ) {
-      ## if current gene has DE result
-      push ( @{$features_hash{$F[0]}{log2FC}}, $F[6] );
-      push ( @{$features_hash{$F[0]}{padj}}, $F[10] );
 
-      ## can't take log of 0 (Inf), so replace with some very small number
-      if ($F[10] == 0) {
-        push ( @{$features_hash{$F[0]}{negLogPadj}}, -log(5e-324)/log(10) );
-      } else {
-        push ( @{$features_hash{$F[0]}{negLogPadj}}, -log($F[10])/log(10) ); ## base-N log of a number is equal to the natural log of that number divided by the natural log of N
+    ## if current gene has DE result
+    push ( @{$features_hash{$F[0]}{log2FC}}, $F[6] );
+    push ( @{$features_hash{$F[0]}{padj}}, $F[10] );
+
+    ## can't take log of 0 (Inf), so replace with some very small number
+    if ($F[10] == 0) {
+      push ( @{$features_hash{$F[0]}{negLogPadj}}, -log(5e-324)/log(10) );
+    } else {
+      push ( @{$features_hash{$F[0]}{negLogPadj}}, -log($F[10])/log(10) ); ## base-N log of a number is equal to the natural log of that number divided by the natural log of N
+    }
+
+    ## is feature DE based on thresholds?
+    if ( $F[10] < $padj_threshold ) {
+      ## feature is significant
+      if ( abs($F[6]) > $logfc_threshold ) {
+        ## feature is DE
+        push ( @{$features_hash{$F[0]}{is_DE}}, "1" );
+        ## feature is sig up-regulated
+        if ( $F[6] > $logfc_threshold ) {
+          push ( @{$features_hash{$F[0]}{is_DE_up}}, "1" );
+        } else {
+          push ( @{$features_hash{$F[0]}{is_DE_up}}, "0" );
+        }
+        ## feature is sig down-regulated
+        if ( $F[6] < -$logfc_threshold ) {
+          push ( @{$features_hash{$F[0]}{is_DE_down}}, "1" );
+        } else {
+          push ( @{$features_hash{$F[0]}{is_DE_down}}, "0" );
+        }
       }
+    } else {
+      push ( @{$features_hash{$F[0]}{is_DE}}, "0" );
+    }
 
-      ## is DE expressed based on thresholds
-
-    # } else {
-    #   ## if not, populate with NAs
-    #   push ( @{$features_hash{$F[0]}{log2FC}}, "NA" );
-    #   push ( @{$features_hash{$F[0]}{padj}}, "NA" );
-    #   push ( @{$features_hash{$F[0]}{negLogPadj}}, "NA" );
-    # }
   }
   close $fh;
 }
