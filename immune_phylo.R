d0a6f8263db91645a5422927a1333a9afb92d0fc
Reuben W Nowell <reubennowell@gmail.com>
2024-06-05 11:49:33 +0100

    update te data

--- immune_phylo.R
+++ immune_phylo.R
@@ -7,21 +7,14 @@ library(cowplot)
 ## cols
 Av.col<-"#F44B19"
 Av.col.light<-"#F9A085"
-Av.col.vlight<-"#FDD8CE"
-Av.col.alpha<-"#F44B1995"
 Ar.col<-"#01AAEA"
 Ar.col.light<-"#80DDFF"
-Ar.col.vlight<-"#CCF1FF"
-Ar.col.alpha<-"#01AAEA95"
-control.col<-"#E6C39F"
-control.col.alpha<-"#E6C39F95"
-hgt.col<-"#1C376D"
 plant.col<-"#66C2A3"
 fungi.col<-"#FFD92E"
 archaea.col<-"#8C9ECA"
 bacteria.col<-"#E68AC2"
 virus.col<-"#FA8C61"
-fake.col<-rgb(0,0,0, max=255, alpha=0)
+# fake.col<-rgb(0,0,0, max=255, alpha=0)
 
 ## read trees
 tree.2_5_RNA_ligase2<-read.tree("data/trees/2_5_RNA_ligase2.combined.hmmalign.clustal.treefile.tax.treefile.UFBoot")
@@ -32,15 +25,11 @@ tree.Glyco_hydro_64<-read.tree("data/trees/Glyco_hydro_64.combined.hmmalign.clus
 tree.Peptidase_C14<-read.tree("data/trees/Peptidase_C14.combined.hmmalign.clustal.treefile.tax.treefile.UFBoot")
 
 plot.2_5_RNA_ligase2<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,1,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
-  plot(tree.2_5_RNA_ligase2, type="u", show.tip.label=F, no.margin=T, edge.color="grey50")
-  legend("topleft", title="2_5_RNA_ligase2", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  plot(tree.2_5_RNA_ligase2, type="u", show.tip.label=F, no.margin=T, edge.color="grey50", rotate.tree=50)
+  legend("topright", title="2_5_RNA_ligase2", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
-  ## bootstrap labels > 95
-  boots <- as.data.frame(cbind(value=as.numeric(as.character(tree.Glyco_hydro_16$node.label)), old.cols=rep("black", length(tree.Glyco_hydro_16$node.label))))
-  boots$edge.cols <- ifelse(is.na(boots$value), fake.col, ifelse(as.numeric(as.character(boots$value))<=95, fake.col, "black"))
-  nodelabels(text=boots$value, adj=c(1.1,-0.5), cex=0.5, col=boots$edge.cols, bg="white", frame="none")
   ## plot tip labels for ref sequences
   tiplabels(tip=grep("AVAG",tree.2_5_RNA_ligase2$tip.label), pch=16, col=Av.col)
   tiplabels(tip=grep("ARIC",tree.2_5_RNA_ligase2$tip.label), pch=16, col=Ar.col)
@@ -49,53 +38,39 @@ plot.2_5_RNA_ligase2<-~{
   tiplabels(tip=grep("Eukaryota",tree.2_5_RNA_ligase2$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.2_5_RNA_ligase2$tip.label), pch=16, col=fungi.col)
   tiplabels(tip=grep("Viridiplantae",tree.2_5_RNA_ligase2$tip.label), pch=16, col=plant.col)
-  tiplabels(tip=grep("Virus",tree.2_5_RNA_ligase2$tip.label), pch=17, col=virus.col)
   ## hilight the upregulated genes
-  tiplabels(tip=grep("AVAG_g20522.t1|AVAG_g35968.t1|AVAG_g36370.t1|AVAG_g41355.t1", tree.2_5_RNA_ligase2$tip.label), pch=21, bg=Av.col)
-  tiplabels(tip=grep("ARIC_g30292.t1|ARIC_g41990.t1|ARIC_g44867.t1", tree.2_5_RNA_ligase2$tip.label), pch=21, bg=Ar.col)
+  tiplabels(tip=grep("AVAG_g20522.t1|AVAG_g35968.t1|AVAG_g36370.t1|AVAG_g41355.t1", tree.2_5_RNA_ligase2$tip.label), pch=24, bg=Av.col)
+  tiplabels(tip=grep("ARIC_g30292.t1|ARIC_g41990.t1|ARIC_g44867.t1", tree.2_5_RNA_ligase2$tip.label), pch=24, bg=Ar.col)
   
-  ## legend
-  legend.2_5_RNA_ligase2<-legend("bottomright", c("A. vaga","A. ricciae","Upregulated","Bacteria","Archaea","Virus","Fungi","Plant","Other eukaryote","Metazoa"), text.font=c(3,3,1,1,1,1,1,1,1,1), 
-                                 pch=c(16,16,1,15,18,17,16,16,16,NA), col=c(Av.col,Ar.col,"black",bacteria.col,archaea.col,virus.col,fungi.col,plant.col,"blue","white"), xjust=1, y.intersp=0.9, bg="grey95", box.col=NA, cex=0.6, pt.cex=1)
-  text(legend.2_5_RNA_ligase2$text$x[10],legend.2_5_RNA_ligase2$text$y[10], "M", font=2, col="blue", cex=0.6, adj=c(1.6,0.5))
 }
 
 plot.RNA_ligase<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,2,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
   plot(tree.RNA_ligase, type="u", show.tip.label=F, no.margin=T, edge.color="grey50")
-  legend("topleft", title="RNA_ligase", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  legend("topright", title="RNA_ligase", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
-  ## bootstrap labels > 95
-  boots <- as.data.frame(cbind(value=as.numeric(as.character(tree.Glyco_hydro_16$node.label)), old.cols=rep("black", length(tree.Glyco_hydro_16$node.label))))
-  boots$edge.cols <- ifelse(is.na(boots$value), fake.col, ifelse(as.numeric(as.character(boots$value))<=95, fake.col, "black"))
-  nodelabels(text=boots$value, adj=c(1.1,-0.5), cex=0.5, col=boots$edge.cols, bg="white", frame="none")
   ## plot tip labels for ref sequences
-  tiplabels(tip=grep("AVAG",tree.RNA_ligase$tip.label), pch=16, col=Av.col)
-  tiplabels(tip=grep("ARIC",tree.RNA_ligase$tip.label), pch=16, col=Ar.col)
+  tiplabels(tip=grep("AVAG",tree.RNA_ligase$tip.label), pch=16, col=Av.col.light)
+  tiplabels(tip=grep("ARIC",tree.RNA_ligase$tip.label), pch=16, col=Ar.col.light)
   tiplabels(tip=grep("Bacteria",tree.RNA_ligase$tip.label), pch=15, col=bacteria.col)
   tiplabels(tip=grep("Archaea",tree.RNA_ligase$tip.label), pch=18, col=archaea.col)
   tiplabels(tip=grep("Eukaryota",tree.RNA_ligase$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.RNA_ligase$tip.label), pch=16, col=fungi.col)
-  tiplabels(tip=grep("Viridiplantae",tree.RNA_ligase$tip.label), pch=16, col=plant.col)
   tiplabels(tip=grep("Virus",tree.RNA_ligase$tip.label), pch=17, col=virus.col)
   tiplabels(tip=grep("Metazoa",tree.RNA_ligase$tip.label), pch=15, cex=1, col="white") ## to provide a bit of white padding
   tiplabels(tip=grep("Metazoa",tree.RNA_ligase$tip.label), text="M", bg=NA, frame="none", cex=0.6, col="blue", font=2)
   ## hilight the upregulated genes
-  tiplabels(tip=grep("AVAG_g1010.t1|AVAG_g1175.t1|AVAG_g12478.t1|AVAG_g14631.t1|AVAG_g16346.t1|AVAG_g33630.t1|AVAG_g36693.t1|AVAG_g50235.t1", tree.RNA_ligase$tip.label), pch=21, bg=Av.col)
-  tiplabels(tip=grep("ARIC_g15771.t1|ARIC_g27178.t1|ARIC_g29778.t1|ARIC_g32107.t1|ARIC_g33926.t1|ARIC_g35221.t1|ARIC_g36236.t1|ARIC_g52925.t1|ARIC_g53944.t1|ARIC_g6629.t1|ARIC_g9944.t1", tree.RNA_ligase$tip.label), pch=21, bg=Ar.col)
+  tiplabels(tip=grep("AVAG_g1010.t1|AVAG_g1175.t1|AVAG_g12478.t1|AVAG_g14631.t1|AVAG_g16346.t1|AVAG_g33630.t1|AVAG_g36693.t1|AVAG_g50235.t1", tree.RNA_ligase$tip.label), pch=24, bg=Av.col)
+  tiplabels(tip=grep("ARIC_g15771.t1|ARIC_g27178.t1|ARIC_g29778.t1|ARIC_g32107.t1|ARIC_g33926.t1|ARIC_g35221.t1|ARIC_g36236.t1|ARIC_g52925.t1|ARIC_g53944.t1|ARIC_g6629.t1|ARIC_g9944.t1", tree.RNA_ligase$tip.label), pch=24, bg=Ar.col)
 }
 
 plot.RNA_lig_T4_1<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,2,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
-  plot(tree.RNA_lig_T4_1, type="u", show.tip.label=F, no.margin=T, edge.color="grey50", rotate.tree=45)
-  legend("topleft", title="RNA_lig_T4_1", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  plot(tree.RNA_lig_T4_1, type="u", show.tip.label=F, no.margin=T, edge.color="grey50", rotate.tree=60)
+  legend("topright", title="RNA_lig_T4_1", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
-  ## bootstrap labels > 95
-  boots <- as.data.frame(cbind(value=as.numeric(as.character(tree.Glyco_hydro_16$node.label)), old.cols=rep("black", length(tree.Glyco_hydro_16$node.label))))
-  boots$edge.cols <- ifelse(is.na(boots$value), fake.col, ifelse(as.numeric(as.character(boots$value))<=95, fake.col, "black"))
-  nodelabels(text=boots$value, adj=c(1.1,-0.5), cex=0.5, col=boots$edge.cols, bg="white", frame="none")
   ## plot tip labels for ref sequences
   tiplabels(tip=grep("AVAG",tree.RNA_lig_T4_1$tip.label), pch=16, col=Av.col)
   tiplabels(tip=grep("ARIC",tree.RNA_lig_T4_1$tip.label), pch=16, col=Ar.col)
@@ -103,69 +78,60 @@ plot.RNA_lig_T4_1<-~{
   tiplabels(tip=grep("Archaea",tree.RNA_lig_T4_1$tip.label), pch=18, col=archaea.col)
   tiplabels(tip=grep("Eukaryota",tree.RNA_lig_T4_1$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.RNA_lig_T4_1$tip.label), pch=16, col=fungi.col)
-  tiplabels(tip=grep("Viridiplantae",tree.RNA_lig_T4_1$tip.label), pch=16, col=plant.col)
   tiplabels(tip=grep("Virus",tree.RNA_lig_T4_1$tip.label), pch=17, col=virus.col)
   ## hilight the upregulated genes
-  tiplabels(tip=grep("AVAG_g11232.t1|AVAG_g12150.t1|AVAG_g27012.t1|AVAG_g30102.t1|AVAG_g40265.t1|AVAG_g8740.t1", tree.RNA_lig_T4_1$tip.label), pch=21, bg=Av.col)
-  tiplabels(tip=grep("ARIC_g15788.t1|ARIC_g30137.t1|ARIC_g36236.t1|ARIC_g43506.t1|ARIC_g9962.t1", tree.RNA_lig_T4_1$tip.label), pch=21, bg=Ar.col)
+  tiplabels(tip=grep("AVAG_g11232.t1|AVAG_g12150.t1|AVAG_g27012.t1|AVAG_g30102.t1|AVAG_g40265.t1|AVAG_g8740.t1", tree.RNA_lig_T4_1$tip.label), pch=24, bg=Av.col)
+  tiplabels(tip=grep("ARIC_g15788.t1|ARIC_g30137.t1|ARIC_g36236.t1|ARIC_g43506.t1|ARIC_g9962.t1", tree.RNA_lig_T4_1$tip.label), pch=24, bg=Ar.col)
 }
 
 plot.Glyco_hydro_16<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,2,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
-  plot(tree.Glyco_hydro_16, type="u", show.tip.label=F, no.margin=T, edge.color="grey50")
-  legend("topleft", title="GH16", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  plot(tree.Glyco_hydro_16, type="u", show.tip.label=F, no.margin=T, edge.color="grey50", rotate.tree=60)
+  legend("topright", title="GH16", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
-  ## bootstrap labels > 95
-  boots <- as.data.frame(cbind(value=as.numeric(as.character(tree.Glyco_hydro_16$node.label)), old.cols=rep("black", length(tree.Glyco_hydro_16$node.label))))
-  boots$edge.cols <- ifelse(is.na(boots$value), fake.col, ifelse(as.numeric(as.character(boots$value))<=95, fake.col, "black"))
-  nodelabels(text=boots$value, adj=c(1.1,-0.5), cex=0.5, col=boots$edge.cols, bg="white", frame="none")
   ## plot tip labels for ref sequences
   tiplabels(tip=grep("AVAG",tree.Glyco_hydro_16$tip.label), pch=16, col=Av.col)
   tiplabels(tip=grep("ARIC",tree.Glyco_hydro_16$tip.label), pch=16, col=Ar.col)
   tiplabels(tip=grep("Bacteria",tree.Glyco_hydro_16$tip.label), pch=15, col=bacteria.col)
   tiplabels(tip=grep("Archaea",tree.Glyco_hydro_16$tip.label), pch=18, col=archaea.col)
-  tiplabels(tip=grep("Eukaryota",tree.Glyco_hydro_16$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.Glyco_hydro_16$tip.label), pch=16, col=fungi.col)
   tiplabels(tip=grep("Viridiplantae",tree.Glyco_hydro_16$tip.label), pch=16, col=plant.col)
-  tiplabels(tip=grep("Virus",tree.Glyco_hydro_16$tip.label), pch=17, col=virus.col)
   ## hilight the upregulated genes
-  tiplabels(tip=grep("AVAG_g10272.t1|AVAG_g2005.t1|AVAG_g5166.t1", tree.Glyco_hydro_16$tip.label), pch=21, bg=Av.col)
-  tiplabels(tip=grep("ARIC_g27738.t1|ARIC_g31373.t1|ARIC_g35326.t1|ARIC_g43249.t1|ARIC_g47117.t1", tree.Glyco_hydro_16$tip.label), pch=21, bg=Ar.col)
+  tiplabels(tip=grep("AVAG_g10272.t1|AVAG_g2005.t1|AVAG_g5166.t1", tree.Glyco_hydro_16$tip.label), pch=24, bg=Av.col)
+  tiplabels(tip=grep("ARIC_g27738.t1|ARIC_g31373.t1|ARIC_g35326.t1|ARIC_g43249.t1|ARIC_g47117.t1", tree.Glyco_hydro_16$tip.label), pch=24, bg=Ar.col)
+
 }
 
 plot.Glyco_hydro_64<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,2,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
   plot(tree.Glyco_hydro_64, type="u", show.tip.label=F, no.margin=T, rotate.tree=60, edge.color="grey50")
-  legend("topleft", title="GH64", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  legend("topright", title="GH64", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
-  ## bootstrap labels > 95
-  boots <- as.data.frame(cbind(value=as.numeric(as.character(tree.Glyco_hydro_16$node.label)), old.cols=rep("black", length(tree.Glyco_hydro_16$node.label))))
-  boots$edge.cols <- ifelse(is.na(boots$value), fake.col, ifelse(as.numeric(as.character(boots$value))<=95, fake.col, "black"))
-  nodelabels(text=boots$value, adj=c(1.1,-0.5), cex=0.5, col=boots$edge.cols, bg="white", frame="none")
   ## plot tip labels for ref sequences
-  tiplabels(tip=grep("AVAG",tree.Glyco_hydro_64$tip.label), pch=16, col=Av.col)
-  tiplabels(tip=grep("ARIC",tree.Glyco_hydro_64$tip.label), pch=16, col=Ar.col)
   tiplabels(tip=grep("Bacteria",tree.Glyco_hydro_64$tip.label), pch=15, col=bacteria.col)
-  tiplabels(tip=grep("Archaea",tree.Glyco_hydro_64$tip.label), pch=18, col=archaea.col)
-  tiplabels(tip=grep("Eukaryota",tree.Glyco_hydro_64$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.Glyco_hydro_64$tip.label), pch=16, col=fungi.col)
-  tiplabels(tip=grep("Viridiplantae",tree.Glyco_hydro_64$tip.label), pch=16, col=plant.col)
-  tiplabels(tip=grep("Virus",tree.Glyco_hydro_64$tip.label), pch=17, col=virus.col)
-  ## hilight the upregulated genes
-  tiplabels(tip=grep("AVAG_g21372.t1|AVAG_g51502.t1", tree.Glyco_hydro_64$tip.label), pch=21, bg=Av.col)
+  ## hilight upregulated genes
+  tiplabels(tip=grep("AVAG_g21372.t1|AVAG_g51502.t1", tree.Glyco_hydro_64$tip.label), pch=24, bg=Av.col)
+  
+  ## legend
+  legend <- legend("left", c("A. vaga","A. ricciae","Upregulated","Downregulated","Bacteria","Archaea","Virus","Fungi","Plant","Other eukaryote","Metazoa"), 
+                   text.font=c(3,3,1,1,1,1,1,1,1,1,1), pch=c(16,16,2,6,15,18,17,16,16,16,NA), 
+                   col=c(Av.col,Ar.col,"black","black",bacteria.col,archaea.col,virus.col,fungi.col,plant.col,"blue","white"), 
+                   xjust=1, y.intersp=0.9, bg="grey95", box.col=NA, cex=0.6, pt.cex=1)
+  text(legend$text$x[11],legend$text$y[11], "M", font=2, col="blue", cex=0.6, adj=c(1.6,0.5))
 }
 
 plot.Peptidase_C14<-~{
-  par(mar=c(0,0,0,0), oma=c(0,0,2,0))
+  par(mar=c(0,0,0,0), oma=c(0,0,0,0))
   ## plot tree
   plot(tree.Peptidase_C14, type="u", show.tip.label=F, no.margin=T, edge.color="grey50", rotate.tree=120)
-  legend("topleft", title="Peptidase_C14", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
+  legend("right", title="Peptidase_C14", c(""), box.col=NA, bg=NA, cex=10/12, inset=0.02) ## add title
   add.scale.bar(font=1, cex=6/10)
   ## plot tip labels for ref sequences
-  tiplabels(tip=grep("AVAG",tree.Peptidase_C14$tip.label), pch=16, col=Av.col.light)
-  tiplabels(tip=grep("ARIC",tree.Peptidase_C14$tip.label), pch=16, col=Ar.col.light)
+  tiplabels(tip=grep("AVAG",tree.Peptidase_C14$tip.label), pch=16, col=Av.col)
+  tiplabels(tip=grep("ARIC",tree.Peptidase_C14$tip.label), pch=16, col=Ar.col)
   tiplabels(tip=grep("Bacteria",tree.Peptidase_C14$tip.label), pch=15, col=bacteria.col)
   tiplabels(tip=grep("Eukaryota",tree.Peptidase_C14$tip.label), pch=20, col="blue")
   tiplabels(tip=grep("Fungi",tree.Peptidase_C14$tip.label), pch=16, col=fungi.col)
@@ -181,5 +147,6 @@ plot.Peptidase_C14<-~{
 }
 
 ## plot
-plot_grid(plot.2_5_RNA_ligase2,plot.RNA_ligase,plot.RNA_lig_T4_1,plot.Glyco_hydro_16,plot.Glyco_hydro_64,plot.Peptidase_C14, nrow=3, labels="auto")
+plot_grid(plot.2_5_RNA_ligase2,plot.RNA_ligase,plot.RNA_lig_T4_1,plot.Glyco_hydro_64,plot.Glyco_hydro_16,plot.Peptidase_C14, 
+          nrow=3, labels=c("auto"))
 
